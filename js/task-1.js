ddb22bdad713c9288e4d0e74991ab4791aed413c
NaumchukSV <130309363+NaumchukSV@users.noreply.github.com>
2024-07-01 19:37:01 +0300

    Completed task 1

---
+++ js/task-1.js
@@ -0,0 +1,15 @@
+const categoriesList = document.querySelector('#categories');
+
+const categoriesItems = categories.querySelectorAll('.item');
+console.log(`Number of categories: ${categoriesItems.length}`);
+
+categoriesItems.forEach(category => {
+    const categoryName = category.querySelector('h2').textContent;
+    const categoryItemsCount = category.querySelectorAll('li').length;
+
+    console.log(`Category: ${categoryName}`);
+    console.log(`Elements: ${categoryItemsCount}`);
+})
+
+
+
